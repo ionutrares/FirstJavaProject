803d94dbf837cbe1d8f710edcc2eb0b8452ac0a3
IonutRares <ionutraresbucur2@gmail.com>
2016-04-24 17:30:41 +0300

    first commit

---
+++ src/day/examples/MyFirstClass.java
@@ -0,0 +1,21 @@
+package day.examples;
+
+public class MyFirstClass {
+
+	/*
+	 * This is my first
+	 *Java program.
+	 * I am new to java.
+	 */
+	public static void main(String[] args) {
+		// My first statemens
+		System.out.println("Hello World");
+
+		int x = 10;
+		int y = 3;
+		int result = x % y;
+		System.out.println("result = " + result);
+		
+	}
+
+}
